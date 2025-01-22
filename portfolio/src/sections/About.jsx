bc6d41b0d36de560841eebfd27aefafacb85caea
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-22 11:25:26 +0530

    updated about section

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -20,13 +20,19 @@ const About = () => {
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img
+              src="assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
 
             <div>
               <p className="grid-headtext">Hi, I’m Shraddha Salvi</p>
               <p className="grid-subtext">
-               2024 graduate, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+                2024 graduate, If you're searching for a web developer, I could
+                be the one! From designing user-friendly interfaces to solving
+                backend challenges, I've honed my skills and am eager to take on
+                new opportunities while continuing to learn and grow..
               </p>
             </div>
           </div>
@@ -34,13 +40,17 @@ const About = () => {
 
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img
+              src="assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
 
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                Frontend Development,Backend Development,Full-Stack Project
+                Experience ,API Integration,Testing & Debugging,Tools like Thunder Client.
               </p>
             </div>
           </div>
@@ -58,12 +68,25 @@ const About = () => {
                 showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                labelsData={[
+                  {
+                    lat: 40,
+                    lng: -100,
+                    text: "Rjieka, Croatia",
+                    color: "white",
+                    size: 15,
+                  },
+                ]}
               />
             </div>
             <div>
-              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Mumbai, India and open to remote work worldwide.</p>
+              <p className="grid-headtext">
+                I’m very flexible with time zone communications & locations
+              </p>
+              <p className="grid-subtext">
+                I&apos;m based in Mumbai, India and open to remote work
+                worldwide.
+              </p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
@@ -71,14 +94,20 @@ const About = () => {
 
         <div className="xl:col-span-2 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
 
             <div>
               <p className="grid-headtext">My Passion for Coding</p>
               <p className="grid-subtext">
-                I love solving problems and building things through code. Programming isn&apos;t just my
-                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
-              </p>
+              I am deeply passionate about coding. Through experimentation, 
+              I realized how much I truly enjoy it—the thrill of writing and 
+              practicing code, constantly thinking about it, and knowing that
+               my skills can eventually serve and impact many people. 
+              It’s a source of genuine happiness for me.</p>
             </div>
           </div>
         </div>
@@ -94,8 +123,13 @@ const About = () => {
             <div className="space-y-2">
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
-                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">shraddhasalvi272@gmail.com</p>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  shraddhasalvi272@gmail.com
+                </p>
               </div>
             </div>
           </div>
